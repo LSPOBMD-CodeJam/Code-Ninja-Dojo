daefcd9e124d606fb2af3a55ba48e60777d81e45
cuipengfei <cuipengfei2008@gmail.com>
2012-08-28 22:16:47 +0800

    one more test for init function

--- ninjas/Pengfei/widgetsSpec.js
+++ ninjas/Pengfei/widgetsSpec.js
@@ -1,45 +1,51 @@
 describe("widgets", function(){
 
-    beforeEach(function(){
-        var input = $('<input type="text" name="widgets" value="" id="widgets"/>');
-        var select = $('<select name="colour" id="colour" size="1" disabled="disabled"/>');
+	beforeEach(function(){
+		var input = $('<input type="text" name="widgets" value="" id="widgets" />');
+		var select = $('<select name="colour" id="colour" size="1" />');
 
-        $(document.body).append(input);
-        $(document.body).append(select);
+		$(document.body).append(input);
+		$(document.body).append(select);
 
-        validateWidgets();
-    });
-
-    afterEach(function(){
-        $("#widgets").remove();
-        $("#colour").remove();
-    });
-
-	it("1 should equal 1", function(){
-		expect(1).toEqual(1);
+		validateWidgets();
 	});
-	
-	it("should enable when the value is 1", function(){
+
+	afterEach(function(){
+		$("#widgets").remove();
+		$("#colour").remove();
+	});
+
+	it("init should disable colour element", function(){
+		var beforeInit = $("#colour").attr("disabled");
+		expect(beforeInit).toEqual(undefined);
+
+		init();
+
+		var afterInit = $("#colour").attr("disabled");
+		expect(afterInit).toEqual("disabled");
+	});
+
+	it("colour should enable when the value is 1", function(){
 		$("#widgets").val(1);
 		$("#widgets").trigger("keyup");
-		
+
 		var actual = $("#colour").attr("disabled");
 		expect(actual).toEqual(undefined);
 	});
 
-	it("should be diabled when the value is 0", function(){
-    		$("#widgets").val(0);
-    		$("#widgets").trigger("keyup");
+	it("colour should be diabled when the value is 0", function(){
+		$("#widgets").val(0);
+		$("#widgets").trigger("keyup");
 
-    		var actual = $("#colour").attr("disabled");
-    		expect(actual).toEqual("disabled");
-    });
+		var actual = $("#colour").attr("disabled");
+		expect(actual).toEqual("disabled");
+	});
 
-	it("should be diabled when the value is -1", function(){
-    		$("#widgets").val(-1);
-    		$("#widgets").trigger("keyup");
+	it("colour should be diabled when the value is -1", function(){
+		$("#widgets").val(-1);
+		$("#widgets").trigger("keyup");
 
-    		var actual = $("#colour").attr("disabled");
-    		expect(actual).toEqual("disabled");
-    });
+		var actual = $("#colour").attr("disabled");
+		expect(actual).toEqual("disabled");
+	});
 })
